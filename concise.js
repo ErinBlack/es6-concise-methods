097ae1eb5656ddbf5d47ccafef77a0bcacc58c16
Erin Black <erin.black@fieldnation.com>
2018-07-05 12:11:48 -0500

    wrote both es5 and es6 concise method types

--- concise.js
+++ concise.js
@@ -1,3 +1,15 @@
+
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('')
+});
+
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
+
 // concise method with es5
 var a = {
     b: function(){
@@ -10,3 +22,17 @@ var a = {
 
 a.b();
 a.c();
+
+// concise method in es6 
+
+var d = {
+    e() {
+        console.log('in e');
+    },
+    f () {
+        console.log('in f');
+    }
+}
+
+d.e();
+d.f();
